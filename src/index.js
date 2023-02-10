272a1665a5965018981f657ea3bf1557bce3a8de
SergeyTimohin <timohinsermain@gmail.com>
2023-02-11 01:48:58 +0300

    complete this task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  if (str.length%2 !== 0) {    
+    return false;
+  }
+
+  let flatBracketsConfig = bracketsConfig.flat();
+  let newBracketsConfig = [];
+  for (let i = 0; i < flatBracketsConfig.length; i +=2) {
+    newBracketsConfig.push(flatBracketsConfig[i] + flatBracketsConfig[i+1]);
+  }
+
+  let deleteSubstr = function(item) {
+    if(str.includes(item)) {
+      str = str.split(item).join('');      
+    }
+  };
+
+  while(newBracketsConfig.some(substr => {
+    return str.includes(substr);
+  })){
+    newBracketsConfig.forEach(elem => {
+      deleteSubstr(elem);
+    });
+  }
+    
+  if (str.length === 0) {  
+    return true;    
+  } else {
+    return false;
+  }
+};
